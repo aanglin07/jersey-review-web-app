40339dd7f4ddea631e1a6449767fb4a94caa1bff
aanglin07 <105037865+aanglin07@users.noreply.github.com>
2023-07-11 11:45:34 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from "./footer"
+
+export default function App() {
+  return (
+    <main>
+      <div>
+                 
+      <Footer />
+      </div>
+      
+      
+    </main>
+  )
+}
