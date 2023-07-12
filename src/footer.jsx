ce3c9df3ccb304fe8e5cd73eafccdb2da59f7ef7
aanglin07 <105037865+aanglin07@users.noreply.github.com>
2023-07-12 10:23:56 +0000

    Updated Footer display responsiveness

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return(
   <div>
     <div className = "footer">
-    <div className = "footer_copyright"><p>{"©" + currentYear} Jersey Review.com. All rights reserved.</p></div>
-    <div className="socialMedia">
-    <p><img  src="https://drive.google.com/uc?          export=view&id=1Z4UmrfwgYuWePhdWp40MbbFczjFtXehv" alt=""/></p>
-    <p><img  src="https://drive.google.com/uc?export=view&id=1uoPJrlDzX7SeXMTXufJTQZDtDJmN8RgR" alt=""/></p>
-    <p><img  src="https://drive.google.com/uc?export=view&id=1RL_JElcMi9jRcKyn-WfFQIHeuf_6rzOh" alt=""/></p>
-    </div>
+      <div className = "footer_copyright"><p>{"©" + currentYear} Jersey Review.com. All rights reserved.</p></div>
+      <div className="socialMedia">
+      <p><img  src="https://drive.google.com/uc?          export=view&id=1Z4UmrfwgYuWePhdWp40MbbFczjFtXehv" alt=""/></p>
+      <p><img  src="https://drive.google.com/uc?export=view&id=1uoPJrlDzX7SeXMTXufJTQZDtDJmN8RgR" alt=""/></p>
+      <p><img  src="https://drive.google.com/uc?export=view&id=1RL_JElcMi9jRcKyn-WfFQIHeuf_6rzOh" alt=""/></p>
+      </div>
   </div>
 </div>
   
